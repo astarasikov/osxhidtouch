8199a3fa68b2f300368cb3014c6f9698f3dabb27
Alexander Tarasikov <alexander.tarasikov@gmail.com>
2013-11-21 23:17:08 +0400

    Initial Commit

---
+++ HidSample/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  HidSample
+//
+//  Created by Alexander Tarasikov on 21.11.13.
+//  Copyright (c) 2013 Alexander Tarasikov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
